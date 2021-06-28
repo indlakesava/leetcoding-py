0bbfc903b2103052e2e29bcd9151d5897afeaf45
kesava reddy <indla.kesavareddy@gmail.com>
2021-06-28 19:32:51 -0400

    First commit, first question Added

---
+++ 1_problem_1880.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        first_value = '';
+        second_value = '';
+        target_value = '';
+        
+        for i in firstWord:
+            first_value += str(ord(i)-97)
+        first_value = int(first_value)
+        
+        for i in secondWord:
+            second_value += str(ord(i)-97)
+        second_value = int(second_value)
+        
+        for i in targetWord:
+            target_value += str(ord(i)-97)
+        target_value = int(target_value)
+        
+        return first_value + second_value == target_value
