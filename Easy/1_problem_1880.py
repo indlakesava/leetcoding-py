79bc349296d6667ead8016ae21126a0893829553
indlakesava <50535274+indlakesava@users.noreply.github.com>
2022-12-05 09:48:02 -0700

    Day 1

--- Easy/1_problem_1880.py
+++ Easy/1_problem_1880.py
@@ -1,5 +1,11 @@
 #https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
 
+
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        return int(''.join([str(ord(a)-97) for a in targetWord])) == int(''.join([str(ord(a)-97) for a in firstWord])) + int(''.join([str(ord(a)-97) for a in secondWord]))
+        
+        
 class Solution:
     def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
         first_value = '';
